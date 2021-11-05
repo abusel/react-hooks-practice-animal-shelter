b5e84506ef416cdf08b1b238ad397b3c7fae772c
Andrew Busel <buselax@gmail.com>
2021-11-05 10:48:20 -0400

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
+
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+  useEffect(()=>{
+   fetchData()
+  },[]
+    )
+    function fetchData(){
+      filters.type === 'all' ?
+      fetch('http://localhost:3000/pets')
+      .then(res=> res.json())
+      .then(data=>setPets(data))
+      :
+      fetch(`http://localhost:3000/pets?type=${filters.type}`)
+      .then(res=> res.json())
+      .then(data=>setPets(data))
+    }
 
   return (
     <div className="ui container">
@@ -15,10 +30,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters filters={filters} setFilters={setFilters} fetchData={fetchData}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets= {pets}/>
           </div>
         </div>
       </div>
